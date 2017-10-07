bbabcbef8cd42ae0d6f329b7caa26ae7c124da61
Florian Windolf <florianwindolf@gmail.com>
2017-10-07 12:47:55 +0200

    Make normalize kernel private to reduce.cu again

--- framework/include/reduce.h
+++ framework/include/reduce.h
@@ -2,12 +2,9 @@
 #define REDUCE_H
 
 #include "cuda_runtime.h"
-#include <iostream>
 
 
 // Define the function structure for reduces
-//typedef float (*reduce_fn)(float, float);
-
 
 /**
  * Normalize the data in Data to interval [o_min, o_max] for one channel
@@ -15,12 +12,4 @@
 __host__ void normalize(float *Data, int w, int h, float o_min, float o_max);
 
 
-/**
- * Kernel for normalizing the Data array with linear transform
- * d = a * d + b
- */
-__global__ void g_normalize(float * Data, int w, int h, float a, float b);
-
-
 #endif // REDUCE_H
-
